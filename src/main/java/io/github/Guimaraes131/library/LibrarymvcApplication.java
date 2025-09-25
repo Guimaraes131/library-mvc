59ecb79e338646db48063ebe7163d5e52c0e28ae
Guilherme <guilhermegpereira26@hotmail.com>
2025-09-25 19:01:16 -0300

    chore: initial commit

---
+++ src/main/java/io/github/Guimaraes131/library/LibrarymvcApplication.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Guimaraes131/library/LibrarymvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
