826c14d652215f385559514c89067e290e981cfc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-26 17:21:49 -0300

    feat: add authors insert migration

---
+++ src/main/resources/db/migration/V4__insert_authors.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_author (name, nationality) VALUES
+('Lucas Monteiro', 'Brasileiro'),
+('Ariana Solis', 'Mexicana'),
+('Ethan Blackwood', 'Canadense'),
+('Sophia Lindberg', 'Sueca'),
+('Khalid Rahman', 'Paquistanês');
